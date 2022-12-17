882b22c4a75348bd8a57e34b3342a06b45329717
Lautaro261 <nahuelygoofy@gmail.com>
2022-12-17 16:07:42 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Cards from './componentes/Cards.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Cards text='Esto es una props'></Cards>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Cards.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Cards = (props)=>{
+
+    return(
+        <div>
+            <h1> Estamos redi Pa </h1>
+            <p>{props.text}</p>
+        </div>
+    )
+
+}
+
+export default Cards;
